b69b1015be62abfb2a5fc84aaea9880c74ab75f0
Yinan He <yinan@boslogin07.rc.fas.harvard.edu>
2025-11-24 02:14:59 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # cs2821r-project
+
+## Run
+
+### Setup
+
+Create submodule HippoRAG (don't need to run this)
+
+```sh
+# First fork HippoRAG on its GitHub repo page
+
+git submodule add https://github.com/<USERNAME>/HippoRAG HippoRAG
+git commit -m "Add HippoRAG submodule"
+git submodule update --init --recursive  # Get all submodules ready
+```
+
+Python environment (run this)
+
+```sh
+mamba create -n hipporag python=3.10
+pip install -r HippoRAG/requirements.txt
+```
+
+### Test
+
+Run VLLM server on GPU.
+
+```sh
+salloc --partition gpu_test --gpus 1 --time 00:30:00 --mem=32G --cpus-per-task=4
+module load python
+source activate hipporag
+CUDA_VISIBLE_DEVICES=0 vllm serve Qwen/Qwen3-8B   --dtype bfloat16   --max-model-len 12288   --gpu-memory-utilization 0.94
+```
+
+Running `test_vllm.py` requires more than 20GB of GPU memory, so use CPU partition or local.
+
+Setup python environment again on CPU only device.
+
+```sh
+python test_vllm.py
+```
+
+If you're running on local device, forward VLLM server's port.
+
+```sh
+ssh -L 8000:localhost:8000 <USERNAME>@login.rc.fas.harvard.edu \
+  -t "ssh -L 8000:localhost:8000 <USERNAME>@<gpu_node_id>.rc.fas.harvard.edu"
+```
---
+++ test_vllm.py
@@ -0,0 +1,62 @@
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "HippoRAG", "src"))
+from hipporag import HippoRAG
+from hipporag.utils.config_utils import BaseConfig
+
+
+save_dir = "outputs"  # Define save directory for HippoRAG objects (each LLM/Embedding model combination will create a new subdirectory)
+llm_model_name = "Qwen/Qwen3-8B"  # Any OpenAI model name
+embedding_model_name = "nvidia/NV-Embed-v2"  # Embedding model name (NV-Embed, GritLM or Contriever for now)
+llm_base_url= "http://holygpu7c26105:8000/v1"  # Base url for your deployed LLM (i.e. http://localhost:8000/v1)
+docs = [
+    "Oliver Badman is a politician.",
+    "George Rankin is a politician.",
+    "Thomas Marwick is a politician.",
+    "Cinderella attended the royal ball.",
+    "The prince used the lost glass slipper to search the kingdom.",
+    "When the slipper fit perfectly, Cinderella was reunited with the prince.",
+    "Erik Hort's birthplace is Montebello.",
+    "Marina is born in Minsk.",
+    "Montebello is a part of Rockland County."
+]
+
+hipporag = HippoRAG(save_dir=save_dir,
+                    llm_model_name=llm_model_name,
+                    embedding_model_name=embedding_model_name,
+                    llm_base_url=llm_base_url)
+
+# Run indexing
+hipporag.index(docs=docs)
+
+# Separate Retrieval & QA
+queries = [
+    "What is George Rankin's occupation?",
+    "How did Cinderella reach her happy ending?",
+    "What county is Erik Hort's birthplace a part of?"
+]
+
+retrieval_results = hipporag.retrieve(queries=queries, num_to_retrieve=2)
+qa_results = hipporag.rag_qa(retrieval_results)
+
+# Combined Retrieval & QA
+rag_results = hipporag.rag_qa(queries=queries)
+
+# For Evaluation
+answers = [
+    ["Politician"],
+    ["By going to the ball."],
+    ["Rockland County"]
+]
+
+gold_docs = [
+    ["George Rankin is a politician."],
+    ["Cinderella attended the royal ball.",
+    "The prince used the lost glass slipper to search the kingdom.",
+    "When the slipper fit perfectly, Cinderella was reunited with the prince."],
+    ["Erik Hort's birthplace is Montebello.",
+    "Montebello is a part of Rockland County."]
+]
+
+rag_results = hipporag.rag_qa(queries=queries, 
+                              gold_docs=gold_docs,
+                              gold_answers=answers)
